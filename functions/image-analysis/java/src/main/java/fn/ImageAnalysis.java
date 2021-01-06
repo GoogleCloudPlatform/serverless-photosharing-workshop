9fd0b8df822cc77fd0aef0f197bc16ef9dfc5635
Guillaume Laforge <glaforge@gmail.com>
2021-01-06 09:20:43 +0000

    Tidy Java code, ensure data is written to Firestore

--- functions/image-analysis/java/src/main/java/fn/ImageAnalysis.java
+++ functions/image-analysis/java/src/main/java/fn/ImageAnalysis.java
@@ -10,6 +10,7 @@ import com.google.api.core.ApiFuture;
 import java.io.*;
 import java.util.*;
 import java.util.stream.*;
+import java.util.concurrent.*;
 import java.util.logging.Logger;
 
 import fn.ImageAnalysis.GCSEvent;
@@ -18,7 +19,8 @@ public class ImageAnalysis implements BackgroundFunction<GCSEvent> {
     private static final Logger logger = Logger.getLogger(ImageAnalysis.class.getName());
 
     @Override
-    public void accept(GCSEvent event, Context context) throws IOException {
+    public void accept(GCSEvent event, Context context) 
+            throws IOException, InterruptedException, ExecutionException {
         String fileName = event.name;
         String bucketName = event.bucket;
 
@@ -55,32 +57,30 @@ public class ImageAnalysis implements BackgroundFunction<GCSEvent> {
             requests.add(request);
 
             logger.info("Calling the Vision API...");
-            BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
-
-            logger.info("Retrieving response list...");
-            List<AnnotateImageResponse> responses = response.getResponsesList();
+            BatchAnnotateImagesResponse result = vision.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = result.getResponsesList();
 
             if (responses.size() == 0) {
                 logger.info("No response received from Vision API.");
                 return;
             }
 
-            AnnotateImageResponse res = responses.get(0);
-            if (res.hasError()) {
-                logger.info("Error: " + res.getError().getMessage());
+            AnnotateImageResponse response = responses.get(0);
+            if (response.hasError()) {
+                logger.info("Error: " + response.getError().getMessage());
                 return;
             }
 
-            logger.info("Annotations found:");
-            List<String> labels = res.getLabelAnnotationsList().stream()
+            List<String> labels = response.getLabelAnnotationsList().stream()
                 .map(annotation -> annotation.getDescription())
                 .collect(Collectors.toList());
+            logger.info("Annotations found:");
             for (String label: labels) {
                 logger.info("- " + label);
             }
 
             String mainColor = "#FFFFFF";
-            ImageProperties imgProps = res.getImagePropertiesAnnotation();
+            ImageProperties imgProps = response.getImagePropertiesAnnotation();
             if (imgProps.hasDominantColors()) {
                 DominantColorsAnnotation colorsAnn = imgProps.getDominantColors();
                 ColorInfo colorInfo = colorsAnn.getColors(0);
@@ -94,8 +94,8 @@ public class ImageAnalysis implements BackgroundFunction<GCSEvent> {
             }
 
             boolean isSafe = false;
-            if (res.hasSafeSearchAnnotation()) {
-                SafeSearchAnnotation safeSearch = res.getSafeSearchAnnotation();
+            if (response.hasSafeSearchAnnotation()) {
+                SafeSearchAnnotation safeSearch = response.getSafeSearchAnnotation();
 
                 isSafe = Stream.of(
                     safeSearch.getAdult(), safeSearch.getMedical(), safeSearch.getRacy(),
@@ -119,7 +119,9 @@ public class ImageAnalysis implements BackgroundFunction<GCSEvent> {
                 data.put("color", mainColor);
                 data.put("created", new Date());
 
-                ApiFuture<WriteResult> result = doc.set(data, SetOptions.merge());
+                ApiFuture<WriteResult> writeResult = doc.set(data, SetOptions.merge());
+
+                logger.info("Picture metadata saved in Firestore at " + writeResult.get().getUpdateTime());
             }
         }
     }
