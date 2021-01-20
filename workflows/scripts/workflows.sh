fa0f9d86b8c24d1d202c57fc3a042083fe4a50b6
Mete Atamel <atamel@google.com>
2021-01-20 11:20:30 +0000

    Added conditional step execution to workflows

--- workflows/scripts/workflows.sh
+++ workflows/scripts/workflows.sh
@@ -5,4 +5,4 @@ set -v
 #gcloud services enable workflows.googleapis.com
 gcloud beta workflows deploy picadaily-workflows --source=workflows.yaml
 gcloud beta workflows execute picadaily-workflows \
-  --data='{"thumbnailUri":"https://thumbnails-service-rqvs6mtotq-ew.a.run.app","gcsImageUri":"gs://uploaded-pictures-workflows-atamel/atamel.jpg","collageUri":"https://collage-service-rqvs6mtotq-ew.a.run.app", "garbageCollectorUri":"https://garbage-collector-service-rqvs6mtotq-ew.a.run.app"}'
+  --data='{"thumbnailUri":"https://thumbnails-service-rqvs6mtotq-ew.a.run.app","collageUri":"https://collage-service-rqvs6mtotq-ew.a.run.app","garbageCollectorUri":"https://garbage-collector-service-rqvs6mtotq-ew.a.run.app","gcsImageUri":"gs://uploaded-pictures-workflows-atamel/atamel.jpg","eventType":"OBJECT_FINALIZE"}'
