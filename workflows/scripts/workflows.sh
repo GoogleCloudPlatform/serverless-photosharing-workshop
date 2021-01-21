cac8f4d5319c54bb8936d0b8eb46f88cbd0d7f90
Mete Atamel <atamel@google.com>
2021-01-21 11:49:20 +0000

    Integrate image analysis into Workflows #35

--- workflows/scripts/workflows.sh
+++ workflows/scripts/workflows.sh
@@ -5,4 +5,4 @@ set -v
 #gcloud services enable workflows.googleapis.com
 gcloud beta workflows deploy picadaily-workflows --source=workflows.yaml
 gcloud beta workflows execute picadaily-workflows \
-  --data='{"thumbnailUri":"https://thumbnails-service-rqvs6mtotq-ew.a.run.app","collageUri":"https://collage-service-rqvs6mtotq-ew.a.run.app","garbageCollectorUri":"https://garbage-collector-service-rqvs6mtotq-ew.a.run.app","gcsImageUri":"gs://uploaded-pictures-workflows-atamel/atamel.jpg","eventType":"OBJECT_FINALIZE"}'
+  --data='{"thumbnailUri":"https://thumbnails-service-rqvs6mtotq-ew.a.run.app","collageUri":"https://collage-service-rqvs6mtotq-ew.a.run.app","garbageCollectorUri":"https://garbage-collector-service-rqvs6mtotq-ew.a.run.app","transformDataUri":"https://europe-west1-pic-a-daily-298110.cloudfunctions.net/vision-data-transform", "bucket":"uploaded-pictures-workflows-atamel","file":"atamel.jpg","eventType":"OBJECT_FINALIZE"}'
